8cbed6ea66f628fc7635f9d024623b873f89bbac
ShixiangWang <w_shixiang@163.com>
2023-07-31 00:27:21 +0800

    优化fastp的失败检测，（重新/开始）跑 OAK 队列

--- qc/fp.sh
+++ qc/fp.sh
@@ -11,8 +11,17 @@ mkdir -p ${outdir}
 
 for sample in $(cat $fqfile)
 do
-    ${fp} -i ${indir}/${sample}_1.fastq.gz -I ${indir}/${sample}_2.fastq.gz -o ${outdir}/${sample}_1.fastq.gz -O ${outdir}/${sample}_2.fastq.gz \
-        -j ${outdir}/${sample}_fastp.json -h ${outdir}/${sample}_fastp.html -w ${nthreads} --dont_overwrite
+    if [ -f ${outdir}/${sample}_fastp.html ]; then
+        echo "QC file for ${sample} has been detected, skip this sample"
+    else
+        ${fp} -i ${indir}/${sample}_1.fastq.gz -I ${indir}/${sample}_2.fastq.gz -o ${outdir}/${sample}_1.fastq.gz -O ${outdir}/${sample}_2.fastq.gz \
+            -j ${outdir}/${sample}_fastp.json -h ${outdir}/${sample}_fastp.html -w ${nthreads}
+        
+        if [ $? -ne 0 ]; then
+            echo "Sample ${sample} is failed"
+            echo "${sample}" >> fastp.failed.txt
+        fi
+    fi
 done
 
 if [ -z "$mqc" ]; then
--- run_batch_from_qc/EGA_OAK/oak_call.sh
+++ run_batch_from_qc/EGA_OAK/oak_call.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-#../common/ll_fq.py ${indir} --output ${fqfile}
+PIPELINE=/home/circrna/circrna-pipeline
 
-fqfile=/home/data/EGA/OAK/code/OAK_RNA.txt
-indir=/home/data/EGA/OAK/raw
-oudir=/home/data/CIRC_PIPE_RESULTS/EGA_OAK
-nthreads=20
+fqfile=./EGA_OAK.txt
+indir=/home/data/IO_RNA/EGA_OAK/fq
+oudir=/home/data/IO_RNA/circRNA/EGA_OAK
+nthreads=30
+config=/home/circrna/circrna-pipeline/config_zhou2.sh
 
-nohup bash ../caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} &> EGA_OAK.log &
+nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> EGA_OAK_call.log &
