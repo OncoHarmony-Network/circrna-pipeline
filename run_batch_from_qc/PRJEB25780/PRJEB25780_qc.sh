911ce556c023341ee596bcec204e8542254e7f3b
ShixiangWang <w_shixiang@163.com>
2023-08-31 10:03:51 +0800

    update log and new cohort qc

---
+++ run_batch_from_qc/PRJEB25780/PRJEB25780_qc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+py=/home/circrna/miniconda3/bin/python3
+fp=/home/circrna/miniconda3/bin/fastp
+multiqc=/home/circrna/miniconda3/bin/multiqc
+PIPELINE=/home/circrna/circrna-pipeline
+
+fqfile=./PRJEB25780.txt
+indir=/home/zhou/t12a/PRJEB25780/raw
+oudir=/home/zhou/raid/IO_RNA/PRJEB25780/fq
+nthreads=20
+
+${py} ${PIPELINE}/common/ll_fq.py ${indir} --output ${fqfile}
+
+nohup bash ${PIPELINE}/qc/fp.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${fp} ${multiqc} &> PRJEB25780_qc.log &
