69ae9f51165b3e3a02e78a8ae2be9dd3e0dfc8e9
ShixiangWang <w_shixiang@163.com>
2023-09-10 14:21:57 +0800

    update log & add 2 cohorts to run

--- run_batch_from_qc/PRJNA476140/PRJNA476140_call.sh
+++ run_batch_from_qc/PRJNA476140/PRJNA476140_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./PRJNA476140.txt
 indir=/home/zhou/raid/IO_RNA/PRJNA476140/fq
 oudir=/home/zhou/raid/IO_RNA/circRNA/PRJNA476140
-nthreads=2
+nthreads=8
 config=/home/circrna/circrna-pipeline/config_zhou.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> PRJNA476140_call.log &
--- run_batch_from_qc/PRJNA940989/PRJNA940989_call.sh
+++ run_batch_from_qc/PRJNA940989/PRJNA940989_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./PRJNA940989.txt
 indir=/home/zhou/raid/IO_RNA/PRJNA940989/fq
 oudir=/home/zhou/raid/IO_RNA/circRNA/PRJNA940989
-nthreads=2
+nthreads=6
 config=/home/circrna/circrna-pipeline/config_zhou.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> PRJNA940989_call.log &
