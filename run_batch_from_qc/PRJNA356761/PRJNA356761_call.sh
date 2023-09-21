9d7aabe133abc460e4259276f300aee0318ff71a
ShixiangWang <w_shixiang@163.com>
2023-09-21 14:25:44 +0800

    Add a new run

--- run_batch_from_qc/PRJNA356761/PRJNA356761_call.sh
+++ run_batch_from_qc/PRJNA356761/PRJNA356761_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./PRJNA356761.txt
 indir=/home/zhou/raid/IO_RNA/PRJNA356761/fq
 oudir=/home/zhou/raid/IO_RNA/circRNA/PRJNA356761
-nthreads=2
+nthreads=10
 config=/home/circrna/circrna-pipeline/config_zhou.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> PRJNA356761_call.log &
