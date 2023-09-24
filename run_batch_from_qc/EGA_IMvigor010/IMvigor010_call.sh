163309ec548c739ccd8a0144ae4eba7e4d2c4322
ShixiangWang <w_shixiang@163.com>
2023-09-24 23:06:17 +0800

    update 010

--- run_batch_from_qc/EGA_IMvigor010/IMvigor010_call.sh
+++ run_batch_from_qc/EGA_IMvigor010/IMvigor010_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./EGA_IMvigor010.txt
 indir=/home/data/IO_RNA/EGA_IMvigor010/fq
 oudir=/home/data/IO_RNA/circRNA/EGA_IMvigor010
-nthreads=6
+nthreads=30
 config=/home/circrna/circrna-pipeline/config_zhou2.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> EGA_IMvigor010_call.log &
