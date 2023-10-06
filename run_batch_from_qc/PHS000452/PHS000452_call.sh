5167a8e30570d2c5e9e42d0a225c9e9dbb435838
ShixiangWang <w_shixiang@163.com>
2023-10-06 23:29:37 +0800

    add 00452 处理的先跑，后续需要续处理

--- run_batch_from_qc/PHS000452/PHS000452_call.sh
+++ run_batch_from_qc/PHS000452/PHS000452_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./PHS000452.txt
 indir=/home/zhou/raid/IO_RNA/PHS000452/fq
 oudir=/home/zhou/raid/IO_RNA/circRNA/PHS000452
-nthreads=8
+nthreads=10
 config=/home/circrna/circrna-pipeline/config_zhou.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> PHS000452_call.log &
