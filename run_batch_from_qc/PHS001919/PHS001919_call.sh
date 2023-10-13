03c0b90934b8c64f450ea70c2c638dc890e20c6e
ShixiangWang <w_shixiang@163.com>
2023-10-13 14:00:50 +0800

    用更多cpu跑1919，加快速度

--- run_batch_from_qc/PHS001919/PHS001919_call.sh
+++ run_batch_from_qc/PHS001919/PHS001919_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./PHS001919.txt
 indir=/home/zhou/raid/IO_RNA/PHS001919/fq
 oudir=/home/zhou/raid/IO_RNA/circRNA/PHS001919
-nthreads=10
+nthreads=30
 config=/home/circrna/circrna-pipeline/config_zhou.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> PHS001919_call.log &
