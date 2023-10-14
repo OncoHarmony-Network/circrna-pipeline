767f8e3779a5f0e4f9462054508f5ec23a0697f2
ShixiangWang <w_shixiang@163.com>
2023-10-14 08:35:08 +0800

    update log

---
+++ run_batch_from_qc/aggr_zhou.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+workdir=/home/zhou/raid/IO_RNA/circRNA
+mkdir ${workdir}/aggr_results
+
+for i in EGA_OAK EGA_IMmotion151 EGA_IMvigor010
+do
+    echo handling $i
+    cp ${workdir}/${i}/aggr/aggr_circRNA.tsv.gz ${workdir}/aggr_results/${i}.aggr_circRNA.tsv.gz 
+    cp ${workdir}/${i}/aggr/${i}.circRNA_all.txt ${workdir}/aggr_results
+done
+
+cd ${workdir}/aggr_results && gzip *.txt
+
+echo Done
