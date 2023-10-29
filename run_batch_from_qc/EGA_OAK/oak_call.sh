a64b416f6b4bb59bee275464b32ed4401b9d5243
ShixiangWang <w_shixiang@163.com>
2023-10-29 13:26:29 +0800

    update zhou2 log & finish all on zhou2

--- run_batch_from_qc/EGA_OAK/oak_call.sh
+++ run_batch_from_qc/EGA_OAK/oak_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./EGA_OAK.txt
 indir=/home/data/IO_RNA/EGA_OAK/fq
 oudir=/home/data/IO_RNA/circRNA/EGA_OAK
-nthreads=30
+nthreads=32
 config=/home/circrna/circrna-pipeline/config_zhou2.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> EGA_OAK_call.log &
