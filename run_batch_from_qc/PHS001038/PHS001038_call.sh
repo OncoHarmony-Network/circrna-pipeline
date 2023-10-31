06e0e903cd4a117db2cb5773535dde6c58eb9ad0
ShixiangWang <w_shixiang@163.com>
2023-10-31 10:03:00 +0800

    update PHS001038 data

--- run_batch_from_qc/PHS001038/PHS001038_call.sh
+++ run_batch_from_qc/PHS001038/PHS001038_call.sh
@@ -5,7 +5,7 @@ PIPELINE=/home/circrna/circrna-pipeline
 fqfile=./PHS001038.txt
 indir=/home/zhou/raid/IO_RNA/PHS001038/fq
 oudir=/home/zhou/raid/IO_RNA/circRNA/PHS001038
-nthreads=4
+nthreads=48
 config=/home/circrna/circrna-pipeline/config_zhou.sh
 
 nohup bash ${PIPELINE}/caller.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${config} &> PHS001038_call.log &
