881af4a9d7784fdc829127ac2be75c46130a8a59
ShixiangWang <w_shixiang@163.com>
2024-01-18 14:11:16 +0800

    PRJNA795330

---
+++ run_batch_from_qc/PRJNA795330/PRJNA795330_qc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+py=/home/circrna/miniconda3/bin/python3
+fp=/home/circrna/miniconda3/bin/fastp
+multiqc=/home/circrna/miniconda3/bin/multiqc
+PIPELINE=/home/circrna/circrna-pipeline
+
+fqfile=./PRJNA795330.txt
+indir=/home/zhou/raid/PRJNA795330/raw
+oudir=/home/zhou/raid/IO_RNA/PRJNA795330/fq
+nthreads=20
+
+${py} ${PIPELINE}/common/ll_fq.py ${indir} --output ${fqfile}
+
+nohup bash ${PIPELINE}/qc/fp.sh ${fqfile} ${indir} ${oudir} ${nthreads} ${fp} ${multiqc} &> PRJNA744780_qc.log &
